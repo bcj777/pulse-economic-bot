1e24b812a12a1e9b755ce7b6609a08dc54c96bd9
bcj777 <132755042+bcj777@users.noreply.github.com>
2026-05-24 01:36:35 +0300

    Update users_db.py

--- users_db.py
+++ users_db.py
@@ -1,18 +1,18 @@
 import json
 import os
 
-DB_PATH = "users.json"
+DB_FILE = "users.json"
 
 
 def init_db():
-    if not os.path.exists(DB_PATH):
-        with open(DB_PATH, "w") as f:
+    if not os.path.exists(DB_FILE):
+        with open(DB_FILE, "w") as f:
             json.dump([], f)
 
 
 def get_users():
     try:
-        with open(DB_PATH, "r") as f:
+        with open(DB_FILE, "r") as f:
             return json.load(f)
     except:
         return []
@@ -24,5 +24,5 @@ def add_user(user_id):
     if user_id not in users:
         users.append(user_id)
 
-        with open(DB_PATH, "w") as f:
+        with open(DB_FILE, "w") as f:
             json.dump(users, f)
